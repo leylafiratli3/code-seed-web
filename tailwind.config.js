e5bdb12c293f4de521d97f7e7dd800c31a31fe84
Bryce <brycelmerrill@gmail.com>
2024-08-25 22:45:34 +0300

    team member fix attempt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#8dba90',
+        secondary: '#5a7d5c',
+        accent: '#D2B48C',
+      },
+      fontFamily: {
+        'fira': ['"Fira Code"', 'monospace'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.backface-hidden': {
+          'backface-visibility': 'hidden',
+        },
+      })
+    },
+  ],
 }
-
